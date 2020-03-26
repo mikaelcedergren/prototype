c07799dc835811c94956d5ba2d2febf9351162c0
Mikael Cedergren <wolfie@mikaelcedergren.com>
2020-03-26 12:15:47 +0100

    script file and added more form fields

--- scripts.js
+++ scripts.js
@@ -1,38 +1,40 @@
 function startsTyping(event) {
-    if (event.target.value.length === 0) return;
-    event.target.nextElementSibling.style.display = 'none';
+  if (event.target.value.length === 0) return;
+  event.target.nextElementSibling.style.display = "none";
 }
 
-const inputs = document.querySelectorAll("input");
+const inputs = document.querySelectorAll("input, textarea");
 [...inputs].forEach(input => {
-    input.addEventListener("focusout", onFocusOut);
-    input.addEventListener("keyup", startsTyping)
-})
+  input.addEventListener("focusout", onFocusOut);
+  input.addEventListener("keyup", startsTyping);
+});
 
 function onFocusOut(event) {
-    inlineValidate(event.target)
+  inlineValidate(event.target);
 }
 
 function inlineValidate(input) {
-    if (input.value == "") {
-        input.nextElementSibling.style.display = "block";
-        return false;
-    }
-    return true;
+  if (input.value == "") {
+    input.nextElementSibling.style.display = "block";
+    return false;
+  }
+  return true;
 }
 
 function globalValidate() {
-    const inputs = document.querySelectorAll("input");
-    const error = [...inputs].reduce((error, currentValue) => {
-        const inputError = !inlineValidate(currentValue)
-        return error || inputError
-    }, false);
+  const inputs = document.querySelectorAll("input, textarea");
+  const error = [...inputs].reduce((error, currentValue) => {
+    const inputError = !inlineValidate(currentValue);
+    return error || inputError;
+  }, false);
 
-    if (error) {
-        document.getElementById("globalError").style.display = "block";
-        setTimeout(function () { document.getElementById("globalError").style.display = "none"; }, 4200);
-        return false;
-    } else {
-        alert("All is well!");
-    }
+  if (error) {
+    document.getElementById("globalError").style.display = "block";
+    setTimeout(function() {
+      document.getElementById("globalError").style.display = "none";
+    }, 4200);
+    return false;
+  } else {
+    alert("All is well!");
+  }
 }
