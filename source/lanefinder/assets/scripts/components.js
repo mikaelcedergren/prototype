32d336d9f693f1929a87e213e64f1549a4b41c60
Mikael Cedergren <wolfie@mikaelcedergren.com>
2020-11-13 19:18:07 +0100

    fixed saved as well

--- source/lanefinder/assets/scripts/components.js
+++ source/lanefinder/assets/scripts/components.js
@@ -2,31 +2,40 @@ class ListItem extends HTMLElement {
   constructor() {
     super();
 
-    const unread = this.getAttribute("unread") === "" ? "<span></span>" : "";
+    const unread =
+      this.getAttribute("unread") === ""
+        ? "<unread-marker></unread-marker>"
+        : "";
+    const hrType =
+      this.getAttribute("layout-right") === ""
+        ? "<hr />"
+        : '<hr class="depth" />';
     const unreadClass = this.getAttribute("unread") === "" ? "unread" : "";
+    const layoutRightClass =
+      this.getAttribute("layout-right") === "" ? "layout-right" : "";
     const paragraph = this.getAttribute("paragraph")
       ? "<p>" + this.getAttribute("paragraph") + "</p>"
       : "";
     const tag = this.getAttribute("tag-content")
-      ? '<tag-item class="' +
+      ? "<tag-item " +
         this.getAttribute("tag-color") +
-        '">' +
+        ">" +
         this.getAttribute("tag-content") +
         "</tag-item>"
       : "";
 
     this.innerHTML = `
-      <div class="list-item ${unreadClass}">
+      <div class="list-item ${unreadClass} ${layoutRightClass}">
         <h3>
         ${unread}
         ${this.getAttribute("title")}</h3>
         ${paragraph}
         <div class="list-item-extra">
             ${tag}
-            <span>24 may 2021</span>
+            <span>${this.getAttribute("extra-content")}</span>
         </div>
         </div>
-        <hr class="depth" />
+        ${hrType}
       `;
   }
 }
