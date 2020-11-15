81cb7f93169390e0737a9264d8bf2fd7bdc9e766
Mikael Cedergren <wolfie@mikaelcedergren.com>
2020-11-15 13:24:16 +0100

    fixed ad overlay and generic

--- source/lanefinder/assets/scripts/components.js
+++ source/lanefinder/assets/scripts/components.js
@@ -185,6 +185,50 @@ app.component("icon-button", {
           `,
 });
 
+app.component("ad-overlay", {
+  props: {},
+  template: `
+<div class="ad-overlay">
+  <main>
+    <header>
+      <icon-button data-close>
+        <i class="material-icons">expand_more</i>
+      </icon-button>
+    </header>
+
+    <div>
+      <slot></slot>
+    </div>
+  </main>
+</div>
+          `,
+});
+
+app.component("generic-overlay", {
+  props: {
+    title: {
+      type: String,
+      default: null,
+    },
+  },
+  template: `
+              <div class="generic-overlay">
+                <main>
+                  <header>
+                    <icon-button data-close
+                      ><i class="material-icons">expand_more</i></icon-button
+                    >
+                    <span>{{title}}</span>
+                    <tag-item secondary>242 jobs</tag-item>
+                  </header>
+                  <div>
+                    <slot></slot>
+                  </div>
+                </main>
+              </div>
+          `,
+});
+
 // CHECKBOX
 
 app.component("checkbox-item", {
